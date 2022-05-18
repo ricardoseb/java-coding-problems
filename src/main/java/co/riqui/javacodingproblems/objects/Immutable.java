c3081efe15bd9c8fedbfa9fe5e343a2b7951ed17
Ricardo Quiroga <ricardo.s.quiroga@gmail.com>
2022-05-17 23:35:02 -0400

    added immutable example

--- src/main/java/co/riqui/javacodingproblems/objects/Immutable.java
+++ src/main/java/co/riqui/javacodingproblems/objects/Immutable.java
@@ -1,14 +1,18 @@
 package co.riqui.javacodingproblems.objects;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
+
+import static co.riqui.javacodingproblems.objects.ImmutableUser.getBuilder;
 
 /**
  * @author ricardoquiroga on 16-05-22
  */
 @Slf4j
-public class Inmutable {
+public class Immutable {
 //    Los tipos primitivos son inmutables.
 //    La famosa clase de Java Stringes inmutable (otras clases también son inmutables, por ejemplo Pattern, y LocalDate)
 //    Los arreglos no son inmutables.
@@ -31,5 +35,35 @@ public class Inmutable {
         log.info("Objects.deepEquals: " + Objects.deepEquals(x, z)); //
         log.info("p1 hash code: " + y.hashCode());
         log.info("p2 hash code: " + z.hashCode());
+
+
+        //PROBNADO IMMUTABLEUSER
+        ImmutableUser user = getBuilder("marin21", "hjju9887h").build();
+        ImmutableUser user2 = getBuilder("ionk", "44fef22")
+                .email("ion@gmail.com")
+                .build();
+
+        log.info("user2 = " + user2);
+        log.info("user = " + user);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public final class Point{
+
+//        Una clase inmutable debe respetar varios requisitos, como los siguientes
+        /**
+         * La clase debe marcarse final para suprimir la extensibilidad (otras clases no pueden extender esta clase; por lo tanto, no pueden anular los métodos)
+         *Todos los campos deben declararse private y final(no son visibles en otras clases, y se inicializan solo una vez en el constructor de esta clase)
+         * La clase debe contener un public constructor parametrizado (o un privateconstructor y métodos de fábrica para crear instancias) que inicialice los campos
+         * La clase debe proporcionar captadores para los campos.
+         * La clase no debe exponer a los setters
+         */
+
+        private final double x;
+        private final double y;
+
+        //Si tuvieramos un atributo mutable deberiamos hacer un clon y obtener sus atributos en el constructor y modificar el getter para obtener este clon
+
     }
 }
---
+++ src/main/java/co/riqui/javacodingproblems/objects/ImmutableUser.java
@@ -0,0 +1,65 @@
+package co.riqui.javacodingproblems.objects;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author ricardoquiroga on 17-05-22
+ * Cuando una clase (inmutable o mutable) tiene demasiados campos, requiere un constructor con muchos argumentos.
+ * Cuando algunos de esos campos son obligatorios y otros son opcionales, esta clase necesitará varios constructores
+ * para cubrir todas las combinaciones posibles.
+ */
+
+@Getter
+@ToString
+public final class ImmutableUser {
+
+    private final String nickname;
+    private final String password;
+    private final String firstname;
+    private final String lastname;
+    private final String email;
+    private final Date created;
+
+    private ImmutableUser(UserBuilder builder) {
+        this.nickname = builder.nickname;
+        this.password = builder.password;
+        this.created = builder.created;
+        this.firstname = builder.firstname;
+        this.lastname = builder.lastname;
+        this.email = builder.email;
+    }
+
+    public static UserBuilder getBuilder(
+            String nickname, String password) {
+        return new ImmutableUser.UserBuilder(nickname, password);
+    }
+
+
+    public static final class UserBuilder {
+        private final String nickname;
+        private final String password;
+        private final Date created;
+        private String email;
+        private String firstname;
+        private String lastname;
+
+
+        public UserBuilder(String nickname, String password) {
+            this.nickname = nickname;
+            this.password = password;
+            this.created = new Date();
+        }
+
+        public ImmutableUser build() {
+            return new ImmutableUser(this);
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+    }
+}
